098ab6fceca1d456dbb2ca2c7cda257a1f8220b4
Yoona8 <elena243v@gmail.com>
2022-11-13 17:02:34 +0100

    Remove mock template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-nature-life app is running!');
+    expect(compiled.querySelector('p')?.textContent).toContain('Hello from main!!');
   });
 });
